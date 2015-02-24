7e01b5fa8c4ebd3ccdba347c9ac5a385170358fe
ingmar <i>
2015-02-24 19:27:06 +0100

    added more layouts and usage of bootstrap as in master brench

--- Gemfile
+++ Gemfile
@@ -1,16 +1,20 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.2.15'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
+gem 'rails', '3.2.8'
 gem 'sqlite3'
-gem 'haml', '~> 3.1'
+gem 'haml', '~> 4.0.5'
 gem 'jquery-rails', '~> 2.1.0'
-gem 'selectize-rails', '~> 0.8.1'
+gem 'selectize-rails', '~> 0.8.5'
 gem 'nested_form', '~> 0.3.2'
 gem 'faker'
+gem 'redcarpet', '~> 2.3.0'
+gem 'auto_html', '~> 1.6.0'
+gem 'twitter-typeahead-rails', '~> 0.10.2'
+gem 'thin'
+gem 'less-rails'
+gem 'sass-rails', '>= 3.2'
+gem 'bootstrap-sass', '~> 3.3.3'
+gem 'execjs', '~> 2.3.0'
 
 group :development do
   gem 'guard-rspec', '~> 2.5.4'
@@ -23,11 +27,11 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
+  if RUBY_PLATFORM =~ /mingw/
+    gem 'therubyracer', '0.11.0beta1', :platform  => [:mswin, :mingw]
+  else
+    gem 'therubyracer', :platform  => [:ruby]
+  end
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,20 +4,57 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-// require jquery
-// require jquery-ui
-// require jquery_ujs
-// require jquery_nested_form
-// require jquery-ui-timepicker-addon
-// require bootstrap.min
-// require tmpl
-// require load-image
-// require canvas-to-blob
-// require bootstrap-image-gallery
-// require rails.validations
-// require jwplayer
-// require selectize
-// require_tree .
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery-ui
+//= require jquery_ujs
+//= require jquery_nested_form
+//= require jquery-ui-timepicker-addon
+//= require tmpl
+//= require load-image
+//= require canvas-to-blob
+//= require bootstrap-image-gallery
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+//= require jquery.fileupload-ip
+//= require jquery.fileupload-ui
+//= require jquery.tablesorter
+//= require jquery.markitup
+//= require jwplayer
+//= require selectize
+//= require twitter/typeahead.min
+//= require_tree .
+
+KRNS = {
+    fbshare: function(loc) {
+      window.open(
+        'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(loc), 
+        'facebook-share-dialog', 
+        'width=626,height=436'); 
+    }
+};
 
 $(document).ready(function () {
+  $(".fb-share").click(function(){KRNS.fbshare(location.href);});
+  $(".markItUp").markItUp(myMarkdownSettings);
+  $(".alert-message").alert();
+  $(".alert").alert();
+  setTimeout("$('.alert').fadeOut('slow');", 3000);
+  $('.carousel').carousel();
+  $('.dropdown-menu').find('form').click(function (e){e.stopPropagation();});
+  $('.datumtijdprikker').datetimepicker({dateFormat: 'yy-mm-dd'});
+  $('.datumprikker').datepicker({dateFormat: 'yy-mm-dd'});
+  $('.tijdprikker').timepicker({});
+  $('.monthpicker .collapser').hide();
+  $('.monthpicker .collapser[data-date='+$('.monthpicker').data("selected")+']').show();
+  $('.monthpicker .head').click(function (e){$('.monthpicker .collapser').filter(':visible').hide("slide", function(){$($(e.target).data("toggle")).show("slide");});});
+  $("form").live("nested:fieldAdded", function(event){$(event.field).find('.tijdprikker').removeClass('hasDatepicker').timepicker({});});
 });
+
+$(function(){
+  $("#hide-banner").click(function(){
+    $.post('/hidebanner');
+    $('#banner').collapse();
+  })
+});
+
