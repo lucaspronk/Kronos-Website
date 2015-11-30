3fc0da9a6d7eb01ed9a9eb2453b4463c66f9c7f2
Reinoud Kruithof <reinoud.kruithof@gmail.com>
2015-11-30 15:31:58 +0100

    Added nodejs to bootstrap.sh to fix missing javascript runtime after removing ruby racer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 # Install linux dependencies
 apt-get update
-apt-get install -y sqlite3 curl git libpq-dev ruby1.9.3
+apt-get install -y sqlite3 curl git libpq-dev ruby1.9.3 nodejs
 
 # Install RVM and subsequently Ruby & Rails
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
