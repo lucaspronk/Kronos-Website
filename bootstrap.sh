8dfb20101e8c7529cd078bf9829e2148fe4da7ad
Reinoud Kruithof <reinoud.kruithof@gmail.com>
2016-01-26 19:22:24 +0100

    Added css for responsive behaviour affix menu. Updated Vagrant bootstrap along the way.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,14 @@
 # Install linux dependencies
 apt-get update
-apt-get install -y sqlite3 curl git libpq-dev ruby1.9.3 nodejs
-
-# Install RVM and subsequently Ruby & Rails
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3 --rails
+apt-get install -y sqlite3 libsqlite3-dev curl git libpq-dev ruby1.9.3 nodejs build-essential rubygems-integration
 
 # Add bundler to install bundles
+gem install rubygems-update
+update_rubygems
 gem install rails bundler
 
 # Install bundles
-cd /vagrant & bundle install & bundle update
+cd /vagrant && bundle install
 
 # Start thin server in development mode (default)
 bundle exec thin start
